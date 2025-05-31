9963e4ac233457981d01f4d9d7a0491a47f1dd15
MahmoudRizaee <mahmoudnaeebi2006@gmail.com>
2025-05-31 09:08:52 +0430

    new

---
+++ Python/lesson6Continue.py
@@ -0,0 +1,11 @@
+# ----------------lesson6Continue----------------- #
+# ----------------Loop in Python----------------- #
+'''
+
+'''
+for x in range(1, 11):
+    for y in range(1,11):
+        print(x, "x", y, " = ", x * y)
+
+# for x in range:
+#     p
