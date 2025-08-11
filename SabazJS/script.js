f2e418c870cc590f3696f13c3cc4e1645ee235e9
MahmoudRizaee <mahmoudnaeebi2006@gmail.com>
2025-08-12 00:09:25 +0430

    New
    change

---
+++ SabazJS/script.js
@@ -0,0 +1,66 @@
+// var userInfo = prompt('لطفا نام و نام خانواده گی خود را وارد کنید:', 'محمود نایبی')
+// var userInfonum = Number(userInfo)
+// var userInfonum = +userInfo
+// alert(typeof userInfonum)
+// console.log(typeoof(userInfonum))
+
+let nan = "kjasfkas"
+let cha = Number("kdsjfkas")
+console.log(typeof(cha)) // Number
+
+let x = Number("ksdjfa")
+console.log(x) // NaN
+
+const y = String(45678)
+console.log(y) // "45678"
+console.log(typeof y) // String
+
+const g = Boolean('');
+console.log(g) // false
+
+let hah = 19;
+let bnb = String(hah)
+console.log(bnb) // "19"
+console.log(typeof bnb) // String
+
+let bo = 1;
+let vb = Boolean(bo);
+console.log(vb); // true
+
+let r = "adfadgadfa";
+let vv = Number(r);
+// alert(vv) // NaN
+
+let va = "2354"
+let vaa = +va;
+console.log(typeof vaa)
+let bwe = Boolean(va); // changing String to Boolean
+console.log(bwe);
+
+var a1 = 10
+var a2 = 5
+var a3 = a1 + a2
+console.log(a3) // output 15
+
+var mix = a3 - (a1 + a2)
+console.log(mix)
+
+const first_Q = (1 + 1) * 7;
+var Secound_Q = (3 + 3) + 1;
+let mixIn = first_Q + Secound_Q;
+let change = String(mixIn)
+console.log(typeof change)
+// In the example above:
+    // Three type of Variables.
+    // Operators
+    // Type of Variables
+    // Cascating of Datatypes
+
+// let tempo = prompt("Hi, friends", "I'm black hat.");
+// alert(tempo)
+
+let django = 1 + 'month';
+var total = "Learning Django takes " + django
+console.log(total)
+
+
