0c51b1b2252e9aa9c9cf2baf1f108d5b4fee4109
MahmoudRizaee <mahmoudnaeebi2006@gmail.com>
2025-08-19 22:19:11 +0430

    News
    Practice

--- SabazJS/script.js
+++ SabazJS/script.js
@@ -1,66 +1,185 @@
-// var userInfo = prompt('لطفا نام و نام خانواده گی خود را وارد کنید:', 'محمود نایبی')
-// var userInfonum = Number(userInfo)
-// var userInfonum = +userInfo
-// alert(typeof userInfonum)
-// console.log(typeoof(userInfonum))
+// // var userInfo = prompt('لطفا نام و نام خانواده گی خود را وارد کنید:', 'محمود نایبی')
+// // var userInfonum = Number(userInfo)
+// // var userInfonum = +userInfo
+// // alert(typeof userInfonum)
+// // console.log(typeoof(userInfonum))
 
-let nan = "kjasfkas"
-let cha = Number("kdsjfkas")
-console.log(typeof(cha)) // Number
+// let nan = "kjasfkas"
+// let cha = Number("kdsjfkas")
+// console.log(typeof(cha)) // Number
 
-let x = Number("ksdjfa")
-console.log(x) // NaN
+// let x = Number("ksdjfa")
+// console.log(x) // NaN
 
-const y = String(45678)
-console.log(y) // "45678"
-console.log(typeof y) // String
+// const y = String(45678)
+// console.log(y) // "45678"
+// console.log(typeof y) // String
 
-const g = Boolean('');
-console.log(g) // false
+// const g = Boolean('');
+// console.log(g) // false
 
-let hah = 19;
-let bnb = String(hah)
-console.log(bnb) // "19"
-console.log(typeof bnb) // String
+// let hah = 19;
+// let bnb = String(hah)
+// console.log(bnb) // "19"
+// console.log(typeof bnb) // String
 
-let bo = 1;
-let vb = Boolean(bo);
-console.log(vb); // true
+// let bo = 1;
+// let vb = Boolean(bo);
+// console.log(vb); // true
 
-let r = "adfadgadfa";
-let vv = Number(r);
-// alert(vv) // NaN
+// let r = "adfadgadfa";
+// let vv = Number(r);
+// // alert(vv) // NaN
 
-let va = "2354"
-let vaa = +va;
-console.log(typeof vaa)
-let bwe = Boolean(va); // changing String to Boolean
-console.log(bwe);
+// let va = "2354"
+// let vaa = +va;
+// console.log(typeof vaa)
+// let bwe = Boolean(va); // changing String to Boolean
+// console.log(bwe);
 
-var a1 = 10
-var a2 = 5
-var a3 = a1 + a2
-console.log(a3) // output 15
+// var a1 = 10
+// var a2 = 5
+// var a3 = a1 + a2
+// console.log(a3) // output 15
 
-var mix = a3 - (a1 + a2)
-console.log(mix)
+// var mix = a3 - (a1 + a2)
+// console.log(mix)
 
-const first_Q = (1 + 1) * 7;
-var Secound_Q = (3 + 3) + 1;
-let mixIn = first_Q + Secound_Q;
-let change = String(mixIn)
-console.log(typeof change)
-// In the example above:
-    // Three type of Variables.
-    // Operators
-    // Type of Variables
-    // Cascating of Datatypes
+// const first_Q = (1 + 1) * 7;
+// var Secound_Q = (3 + 3) + 1;
+// let mixIn = first_Q + Secound_Q;
+// let change = String(mixIn)
+// console.log(typeof change)
+// // In the example above:
+//     // Three type of Variables.
+//     // Operators
+//     // Type of Variables
+//     // Cascating of Datatypes
 
-// let tempo = prompt("Hi, friends", "I'm black hat.");
-// alert(tempo)
+// // let tempo = prompt("Hi, friends", "I'm black hat.");
+// // alert(tempo)
 
-let django = 1 + 'month';
-var total = "Learning Django takes " + django
-console.log(total)
+// let django = 1 + 'month';
+// var total = "Learning Django takes " + django
+// console.log(total)
 
 
+// var numric = prompt("Enter Your Name:")
+// var numric1 = prompt("Enter Your Age:")
+
+// // In here you have to change the datatype of this into number:
+// var cascate = Number(numric)
+// var cascate1 = Number(numric1)
+
+// var resulte1 = cascate + cascate1;
+// var resulte2 = cascate - cascate1;
+// var other1 = cascate * cascate1;
+// var other2 = cascate / cascate1;
+
+// alert(other1)
+// alert(typeof resulte1)
+
+
+
+
+
+
+
+
+
+
+//              Concating Strings in JavaScript             //
+var siteName = 'coding.com';
+var courseName = 'JavaScript';
+
+// alert(siteName + " " + courseName); // Output: Coding.com JavaScript
+// alert("Site: " + siteName + "\n" + "Courses: " + courseName)
+// alert('This is \'JavaScript\' Courses from ' + 'Coding.com');
+
+// var str1 = 'Hello Mahmoud '
+// var num1 = 4
+// var num2 = 2
+
+// + - / * **
+
+// alert(str1 * num1) // NaN
+// alert(typeof NaN)
+
+// console.log("12" / 4)
+// alert("12" ** 2)
+
+// var str1 = 'coding.com'
+// var str2 = 'JavaScript'
+
+// var num1 = 12
+// var num2 = 3
+
+// console.log(str2 - num1)
+// console.log(isNaN(num1 / num2)) // true
+
+// var num1 = 12
+
+// var num2 = 3
+
+// var num3 = 4
+
+// var str2 = '3'
+
+// var str3 = '4'
+
+// console.log(num1 == num2 * num3) // true
+// console.log(num2 * num3 / num1) // true
+// console.log(num2 == str2) // true
+// console.log(num3 === str3) // false
+
+// var num = 11
+
+// var num1 = 3
+
+// var num2 = 4
+
+// if (num1 * num2 === num){
+//     console.log("Yes, these two is equal")
+// }else{
+//     console.log("No, Sorry these two variables are not equal")
+// }
+
+// if (num1 * num2 === num){
+//     num = num + 1
+//     console.log(num)
+// }else{
+//     console.log("Equal")
+// }
+
+
+// // Exercise:
+// // Make a input that get minutes and change it to Hours:
+
+// // First = This is input:
+// var minute = prompt("Enter your time (minute): ", 0);
+
+// // Changing Minute to Hour:
+// var minuteToHour = minute / 60
+// // condition:
+// if (isNaN(minuteToHour)) {
+//     alert("This number is invalid!")
+// }else{
+//     alert(minuteToHour + 'hr')
+// }
+
+
+// var one = Number(prompt("Enter a Number: ", 0));
+// var second = Number(prompt("Enter a Number: ", 0));
+// var thired = Number(prompt("Enter a Number: ", 0));
+
+// var avarage = (one + second + thired) / 3;
+
+// alert(avarage)
+
+// var userAge = Number(prompt('How old are you? ', 0));
+
+// if (userAge < 18 || userAge > 100){
+//     alert('Your not able to access.')
+// }else{
+//     alert('You are ' + userAge + ' years old.')
+// }
