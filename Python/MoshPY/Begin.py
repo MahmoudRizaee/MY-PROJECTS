b47d37e70d5e6152399483d9966707c0c65acdb7
MahmoudRizaee <mahmoudnaeebi2006@gmail.com>
2026-03-31 11:22:50 +0430

    new

---
+++ Python/MoshPY/Begin.py
@@ -0,0 +1,23 @@
+# Vareables
+# A variable is container for values. It is used to store data that can be used later in the program. In Python, you can create a variable by assigning a value to it using the equals sign (=).
+
+age = 30
+price = 19.99
+first_name = "John"
+is_online = False
+print(age)
+
+# Exercises
+# We check in a patient named John Smith.
+# He is 30 years old.
+# He is a new patient.
+pacient_name = "John Smith"
+age = 30
+is_new = True
+print(pacient_name, age , is_new)
+
+name = input("What is your name? ")
+print("Hello " + name) # String concatenation
+birth_year = input("Enter your birth year: ")
+age = 2026 - int(birth_year) # Convert string to integer
+print("Your age is: " + str(age)) # Convert integer to string for concatenation
